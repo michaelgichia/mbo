29f817093e1d840def4742d0da42989d9edd3f49
michaelgichia <gichuru.gichia@gmail.com>
2018-01-11 14:57:43 +0300

    Improve UI.

--- app/components/LoadingIndicator/index.js
+++ app/components/LoadingIndicator/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import { Spin } from 'antd';
-
-
+import FullpageIndicator, { PlainIndicator } from './FullpageIndicator';
 // Css
 import './loading-indicator.css';
-
-import Circle from './Circle';
-import Wrapper from './Wrapper';
-import FullpageIndicator, { PlainIndicator } from './FullpageIndicator';
-
-const LoadingIndicator = () => (
-  <Wrapper>
-    <Circle />
-    <Circle rotate={30} delay={-1.1} />
-    <Circle rotate={60} delay={-1} />
-    <Circle rotate={90} delay={-0.9} />
-    <Circle rotate={120} delay={-0.8} />
-    <Circle rotate={150} delay={-0.7} />
-    <Circle rotate={180} delay={-0.6} />
-    <Circle rotate={210} delay={-0.5} />
-    <Circle rotate={240} delay={-0.4} />
-    <Circle rotate={270} delay={-0.3} />
-    <Circle rotate={300} delay={-0.2} />
-    <Circle rotate={330} delay={-0.1} />
-  </Wrapper>
-);
-
-export default LoadingIndicator;
 
 export const PlainLoader = () => (
   <PlainIndicator>
@@ -39,4 +15,14 @@ export const BubblingLoader = () => (
      <Spin size="large" />
   </FullpageIndicator>
 );
+
+export default function LoadingIndicator() {
+  return (
+    <div className="wrapper">
+      <Spin size="large" />
+    </div>
+  );
+}
+
+
 
